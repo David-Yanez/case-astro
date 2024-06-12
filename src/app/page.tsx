bbd41931cc204697bfbab5d9b123c73b7b9ffa08
David-Yanez <luis.yanez@epn.edu.ec>
2024-06-11 22:10:18 -0500

    fixed title styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,12 @@ export default function Home() {
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32
          lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
           <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
-          <div className="retative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+          <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
             <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+            <div className='absolute inset-x-0 bottom-0 bg-gradient-to-t via-slate-50/50 from-slate-50 h-28' />
               <img src="/snake-1.png" className="w-full" />
             </div>
-            <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900
-            text-5xl md:text-6xl lg:text-7xl">Tu Imagen en un <span className=" bg-blue-600 px-2 text-white">Personalizado</span> Estuche de Celular</h1>
+            <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">Tu Imagen en un <span className=" bg-blue-600 px-2 text-white">Personalizado</span> Estuche de Celular</h1>
             <p className="mt-8 text-lg lg:pr-10 max-w-prose text-center lg:text-left text-balance md:text-wrap">
               Captura tus recuerdos favoritos en tu propio 
               <span className="font-semibold"> estuche de celular</span>. AstroCase proteje tus recuerdos y no solo tu celular

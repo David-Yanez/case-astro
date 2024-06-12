602177463eeab859350619f0be258b49bdeccc03
David-Yanez <luis.yanez@epn.edu.ec>
2024-06-11 21:53:23 -0500

    added home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
-import { Check } from "lucide-react";
+import { Phone } from "@/components/Phone";
+import { Check, Star } from "lucide-react";
 import Image from "next/image";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <section>
         <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32
          lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
           <div className="retative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
             <div className="absolute w-28 left-0 -top-20 hidden lg:block">
               <img src="/snake-1.png" className="w-full" />
@@ -32,10 +34,36 @@ export default function Home() {
                   <Check className="h-5 w-5 shrink-0 text-blue-600"/>
                   Soporta modelos nuevos de iPhone
                 </li>
-
               </div>
-
             </ul>
+              <div className="mt-12 flex flex-col sm:flex-row items-center sm:items-start gap-5">
+                <div className="flex -space-x-4">
+                  <img src="/users/user-1.png" alt="users" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
+                  <img src="/users/user-2.png" alt="users" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
+                  <img src="/users/user-3.png" alt="users" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
+                  <img src="/users/user-4.jpg" alt="users" className="inline-block h-10 w-10 rounded-full ring-2 ring-slate-100" />
+                  <img src="/users/user-5.jpg" alt="users" className="inline-block object-cover h-10 w-10 rounded-full ring-2 ring-slate-100" />
+                </div>
+                <div className="flex flex-col justify-between items-center sm:items-start">
+                  <div className="flex gap-0.5">
+                    <Star className="h-4 w-4 text-blue-600 fill-blue-600 "/>
+                    <Star className="h-4 w-4 text-blue-600 fill-blue-600 "/>
+                    <Star className="h-4 w-4 text-blue-600 fill-blue-600 "/>
+                    <Star className="h-4 w-4 text-blue-600 fill-blue-600 "/>
+                    <Star className="h-4 w-4 text-blue-600 fill-blue-600 "/>
+                  </div>
+                  <p><span className=" font-semibold">1.350</span> clientes felices</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-span-full lg:col-span-1 w-full flex justify-center px-8 sm:px-16 md:px-0 mt-32 lg:mx-0 lg:mt-20 h-fit">
+            <div className="relative md:max-w-xl">
+              <img src="/your-image.png" alt="image" className="absolute w-40 lg:w-52 left-56 -top-20 select-none hidden sm:block lg:hidden xl:block" />
+              <img src="/line.png" alt="line 1" className="absolute w-20 -left-6 -bottom-6 select-none" />
+              <Phone className="w-64" imgSrc="/testimonials/1.jpg"/>
+            </div>
           </div>
          </MaxWidthWrapper>
       </section>

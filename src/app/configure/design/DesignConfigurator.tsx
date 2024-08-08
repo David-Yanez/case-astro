5ed879975e858faef0086f500626ae481b3ef1ac
David-Yanez <luis.yanez@epn.edu.ec>
2024-08-07 20:19:13 -0500

    fix responive issues

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -37,13 +37,13 @@ export const DesignConfigurator = ({configId, imageUrl, imageDimensions}: Design
     })
   
     return (
-    <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
+    <div className="relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20">
         <div className="relative h[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2">
             <div className="relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]">
                 <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full ">
                     <NextImage fill alt='phone image' src="/phone-template.png" className='pointer-events-none z-50 select-none '/>
                 </AspectRatio>
-                <div className="absolute z-40 inset-0 left-[3px] top-px-right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229, 231, 235, 0.6)] "/>
+                <div className="absolute z-40 inset-0 left-[3px] top-px-right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)] "/>
               <div className={cn("absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px]",
               `bg-${options.color.tw}`
               )}/>
@@ -69,7 +69,7 @@ export const DesignConfigurator = ({configId, imageUrl, imageDimensions}: Design
             </Rnd>
             
         </div>
-        <div className="h-[37.5rem] flex flex-col bg-white">
+        <div className="h-[37.5rem] w-full col-span-full lg:col-span-1 flex flex-col bg-white">
             <ScrollArea className="relative flex-1 overflow-auto">
                 <div aria-hidden="true" className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white  pointer-events-none" />
                 <div className="px-8 pb-12 pt-8"   >

34dd54e10b34f2acbd633948e8047088086c91e9
David-Yanez <luis.yanez@epn.edu.ec>
2024-08-19 23:31:31 -0500

    add page preview

---
+++ src/app/configure/preview/DesignPreview.tsx
@@ -0,0 +1,24 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import Confetti from 'react-dom-confetti'
+
+export const DesignPreview = () => {
+    
+    const [showConfetti, setShowconfetti] = useState<boolean>(false)
+    useEffect(() => setShowconfetti(true), [])
+
+  return (
+    <>
+    <div 
+    aria-hidden='true'
+    className='pointer-events-none select-none absolute inset-0 overflow-hidden flex justify-center'>
+        <Confetti active={showConfetti} 
+        config={{elementCount: 200, spread: 90}}/>
+    </div>
+
+    <div className='mt-20 flex flex-col items-center md:grid text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
+        <div className='md:col-span-4 lg:col-span-3 md:row-span-2 md:row-end-2'></div>
+    </div>
+    </>
+  )
+}
